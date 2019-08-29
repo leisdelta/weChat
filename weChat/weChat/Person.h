e980ea1b8893e13c281f16207f62fe9e12e0491e
leilei <18958740095@163.com>
2019-08-30 07:39:57 +0800

    创建Person类

---
+++ weChat/weChat/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  weChat
+//
+//  Created by mushroom on 2019/8/30.
+//  Copyright © 2019 xiaomage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ weChat/weChat/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weChat
+//
+//  Created by mushroom on 2019/8/30.
+//  Copyright © 2019 xiaomage. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
